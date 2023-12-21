cd67828f8febc89cca392fab791327461bd4c64c
ThreeCodiots <threecodiots@gmail.com>
2023-12-22 05:06:08 +0530

    Second Commit

--- Components/Footer.js
+++ Components/Footer.js
@@ -9,7 +9,7 @@ function Footer() {
       <div className="bg-indigo-600 dark:bg-indigo-900">
         <div className="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
           <p className="text-gray-50 text-sm text-center sm:text-left">
-            © {year} Chatify —
+            © {year}  Blogify —
             <a
               href="https://twitter.com/SatyamChoubey77"
               rel="noopener noreferrer"
